26c70e51bf2c8d22a963a8f244da8f4190c69582
chsd60 <laptop@home>
2021-02-14 17:35:44 +0100

    Misc

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
     public bool activePapaGQuest1 = false;
     public bool dreamcatcher = true;
+    public bool scene5SL = true;
     public GameObject papaGQuest1Obj;
     public GameObject papaGQuest1Obj2;
     public GameObject papaGQuest1SceneL;
@@ -12,8 +13,11 @@ public class GameManager : MonoBehaviour {
     public GameObject dreamcatcherObj;
     public GameObject dreamcatcherDial1;
 
+    public GameObject dialogObj;
+    public GameObject sceneLObj;
+
     void Update() {
-        if (activePapaGQuest1 == false) {
+        if (activePapaGQuest1) {
             papaGQuest1Obj.SetActive(true);
             papaGQuest1SceneL.SetActive(false);
             papaGQuest1Obj2.SetActive(false);
@@ -24,11 +28,18 @@ public class GameManager : MonoBehaviour {
             papaGQuest1Obj2.SetActive(true);
         }
 
-        if (dreamcatcher == true) {
+        if (dreamcatcher) {
             dreamcatcherObj.SetActive(true);
         } else {
             dreamcatcherObj.SetActive(false);
             dreamcatcherDial1.SetActive(true);
+        }
+
+        if (scene5SL) {
+            dialogObj.SetActive(true);
+        } else {
+            dialogObj.SetActive(false);
+            sceneLObj.SetActive(true);
         }
     }
 
@@ -38,6 +49,10 @@ public class GameManager : MonoBehaviour {
 
     public void Dreamcatcher() {
         dreamcatcher = false;
+    }
+
+    public void Scene5SL() {
+        scene5SL = true;
     }
 
 }
